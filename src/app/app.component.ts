f98a4346cf01cc998f1a40707ac00cec18e6fca4
807782352 <kyrie807782352@gmail.com>
2024-11-19 15:37:37 -0500

    add a TaskComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,26 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-user';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  user = DUMMY_USERS;
+  users = DUMMY_USERS;
+
+  selectedUserId = 'u1'; // initialize
+
+  get selectedUser() {
+    // ! means I can pretty sure we can get a value in this process
+    return this.users.find((user) => this.selectedUserId === user.id)!;
+  }
 
   onSelectUser(id: string) {
-    console.log('The user Id is: ', id);
+    this.selectedUserId = id;
   }
 }
